a4a2adf2afc5892b627864278212fca3789ed558
Misael <misa_936@hotmail.com>
2024-05-09 10:38:31 -0600

    four commit

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,33 +1,21 @@
-function ArrowIcon() {
-  return (
-    <svg
-      width="12"
-      height="12"
-      viewBox="0 0 12 12"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
-        fill="currentColor"
-      />
-    </svg>
-  )
-}
+import { FaGithub } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { FaLinkedin } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <footer className="mb-16">
+    <footer className="mb-16 mt-16">
       <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
         <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="/rss"
+            href="https://github.com/vercel/next.js"
           >
-            <ArrowIcon />
-            <p className="ml-2 h-7">rss</p>
+            <FaGithub className="h-5 w-5" />
+            <p className="ml-2 h-7 pt-1 font-semibold">Github</p>
           </a>
         </li>
         <li>
@@ -37,8 +25,8 @@ export default function Footer() {
             target="_blank"
             href="https://github.com/vercel/next.js"
           >
-            <ArrowIcon />
-            <p className="ml-2 h-7">github</p>
+            <FaLinkedin className="h-5 w-5" />
+            <p className="ml-2 h-7 pt-1 font-semibold">Linkedin</p>
           </a>
         </li>
         <li>
@@ -46,16 +34,22 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://vercel.com/templates/next.js/portfolio-starter-kit"
           >
-            <ArrowIcon />
-            <p className="ml-2 h-7">view source</p>
+            <SiGmail   className="h-5 w-5"/>
+            <p className="ml-2 h-7 pt-1 font-semibold">christian.misael.fr@gmail.com</p>
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <BsFillTelephoneFill className="h-5 w-5 "/>
+            <p className="ml-2 h-7 pt-1 font-semibold">3318634402</p>
           </a>
         </li>
       </ul>
-      <p className="mt-8 text-neutral-600 dark:text-neutral-300">
-        © {new Date().getFullYear()} MIT Licensed
-      </p>
     </footer>
   )
 }
